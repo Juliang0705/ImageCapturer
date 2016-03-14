e7769da186114ef61430f403dda679be02f46ae1
Juliang0705 <juliang0705@gmail.com>
2016-03-14 01:16:03 -0500

    can get a modal panel now

---
+++ script/background.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Juliang on 3/14/16.
+ */
+
+chrome.browserAction.onClicked.addListener(function (tab) {
+    // for the current tab, inject the "inject.js" file & execute it
+    chrome.tabs.executeScript(tab.ib, {
+        file: 'script/popup.js'
+    });
+});
---
+++ script/popup.js
@@ -0,0 +1,15 @@
+(function() {
+
+  var overlay=document.createElement('div');
+  var oStyle=overlay.style;
+  oStyle.position='absolute';
+  oStyle.backgroundColor='#fff';
+  oStyle.width='400px';
+  oStyle.height='400px';
+  oStyle.top='10px';
+  oStyle.right='10px';
+  oStyle.borderRadius='3px';
+  oStyle.border='1px solid #888';
+  document.body.appendChild(overlay);
+
+})();
