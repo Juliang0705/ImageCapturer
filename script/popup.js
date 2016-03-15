a907f454b34b9551a65683ffb4a8cd84b51cfa51
Juliang0705 <juliang0705@gmail.com>
2016-03-14 23:16:44 -0500

    make panel fixed and closable

--- script/popup.js
+++ script/popup.js
@@ -1,15 +1,34 @@
+
+function createPopUp(){
+    var popup = $('<div></div>')
+        .css({
+            zIndex : 2147483647,
+            position: 'fixed',
+            backgroundColor : '#fff',
+            width: '400px',
+            height : '400px',
+            top: '20px',
+            right : '20px',
+            borderRadius : '10px',
+            border : '1px solid #888'})
+        .appendTo(document.body);
+    return popup;
+}
+
+function addCloseButton(parent){
+    var imgUrl = chrome.extension.getURL("image/close.png");
+    var closeButton = $('<img />',{src: imgUrl, alt: "X"})
+        .css({
+            position: 'absolute',
+            right: '0px'
+        })
+        .on('click',function(){
+            parent.remove();
+        })
+        .appendTo(parent);
+}
+
 (function() {
-
-  var overlay=document.createElement('div');
-  var oStyle=overlay.style;
-  oStyle.position='absolute';
-  oStyle.backgroundColor='#fff';
-  oStyle.width='400px';
-  oStyle.height='400px';
-  oStyle.top='10px';
-  oStyle.right='10px';
-  oStyle.borderRadius='3px';
-  oStyle.border='1px solid #888';
-  document.body.appendChild(overlay);
-
+    var popup = createPopUp();
+    addCloseButton(popup);
 })();
