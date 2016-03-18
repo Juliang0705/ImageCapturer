e0d0db946f635960f39b872ccc33b66d3de4d8e9
Juliang0705 <juliang0705@gmail.com>
2016-03-18 03:21:38 -0500

    add prompt animation

--- script/popup.js
+++ script/popup.js
@@ -2,24 +2,24 @@
     set up the pop up panel
  */
 function createPanel(){
-    var popup = $('<div id="ImageCapturer-Panel"></div>')
+    var panel = $('<div id="ImageCapturer-Panel"></div>')
         .css({
             zIndex : 2147483647,
             position: 'fixed',
-            backgroundColor : '#fff',
+            backgroundColor : '#718C98',
             width: '400px',
             height : '400px',
             top: '20px',
             right : '20px',
             borderRadius : '10px',
-            border : '1px solid #888'})
+            border : '3px solid #FDFCFC'})
         .appendTo(document.body);
-    return popup;
 }
 /*
     set up the close button on the upper right
  */
-function addCloseButton(parent){
+function addCloseButton(){
+    var parent = $('#ImageCapturer-Panel');
     var imgUrl = chrome.extension.getURL("image/close.png");
     var closeButton = $('<img />',{src: imgUrl, alt: "X"})
         .css({
@@ -29,94 +29,145 @@ function addCloseButton(parent){
         .on('click',function(){
             parent.remove();
         })
+        .on('dragstart',function(event){event.preventDefault();})
         .appendTo(parent);
 }
-
-function addImageFrame(parent){
+/*
+    add the frame for image at the top
+ */
+function addImageFrame(){
+    var parent = $('#ImageCapturer-Panel');
     var imageFrame = $('<div id="ImageCapturer-ImageFrame"></div>')
         .css({
             width: '30%',
             height: '30%',
-            position: 'absolute',
             borderRadius: '5px',
-            border : '1px solid #888'
+            margin: '40px auto 0px auto'
         })
         .appendTo(parent);
-    imageFrame.css("left", (parent.width()-imageFrame.width())/2);
-    imageFrame.css("top", (parent.height()-imageFrame.height())/6);
-
-    return imageFrame;
 }
-//
-//function getImgSrc(mObj)
-//{
-//    var ret = '';
-//    if( mObj.length > 1 )
-//    {
-//        $(mObj).each(function(idx,obj){
-//            ret = getImgSrc(obj);
-//            if( ret != '') { return; }
-//        });
-//    }
-//    else if( $(mObj).children().length > 0 )
-//    {
-//        $(mObj).children().each(function(idx,obj){
-//            ret = getImgSrc(obj);
-//            if( ret != '') { return; }
-//        });
-//    }
-//    else if( $(mObj).prop('tagName').toUpperCase() == 'IMG' )
-//    {
-//        ret =  $(mObj).attr('src');
-//    }
-//    return ret;
-//}
+/*
+    the container for image info and the current URL page
+ */
+function addImageDescriptionFrame(){
+    var parent = $('#ImageCapturer-Panel');
+    $('<div id="ImageCapturer-ImageDescription"></div>')
+        .css({
+            width: 'auto',
+            'word-wrap': 'break-word'
+        })
+        .appendTo(parent);
+}
+/*
+    @param - image a JQuery img object
+ */
+String.prototype.filename=function(extension){
+    var s= this.replace(/\\/g, '/');
+    s= s.substring(s.lastIndexOf('/')+ 1);
+    return extension? s.replace(/[?#].+$/, ''): s.split('.')[0];
+}
+function getImageDescription(image){
+    //must be JQuery object and is an image
+    if (!(image instanceof jQuery && image.is('img') === true)){
+        return null;
+    }
+    var altText = image.attr('alt');
+    var filename = image.attr('src').filename();
+    return (altText && altText.length > 0) ? altText : filename;
+}
 
+function addImageTextToPanel(text){
+    $('<p>'+text+'</p>')
+        .css({
+            margin: '10px auto',
+            'text-align': 'center',
+            padding : '0px 5px'
+        })
+        .appendTo($('#ImageCapturer-ImageDescription'));
+}
 
+function addCurrentURL(){
+    var url = window.location.href;
+    $('<a><p>'+ url +'</p></a>')
+        .attr('href',url)
+        .css({
+            margin: '10px auto',
+            'text-align': 'center',
+            padding : '0px 5px'
+        })
+        .appendTo($('#ImageCapturer-ImageDescription'));
+}
+/*
+    animation to prompt the user to drop stuff in it
+ */
+function promptDropAnimation(){
+    function animation (){
+        var newColor = '#'+(0x1000000+(Math.random())*0xffffff).toString(16).substr(1,6);
+        $('#ImageCapturer-Animation').animate({
+            'font-size': '40px',
+            'color': newColor
+        },2000,reverseAnimation);
+    }
+    function reverseAnimation(){
+        $('#ImageCapturer-Animation').animate({
+            'font-size': '20px'
+        },2000,animation);
+    }
+    var parent = $('#ImageCapturer-ImageDescription');
+    $('<div id="ImageCapturer-Animation"></div>')
+        .append($('<p>' + 'Drop an image here!'+ '</p>'))
+        .css({
+            'text-align': 'center',
+            'font-size': '20px',
+        })
+        .appendTo(parent);
+    animation();
+}
+
+/*
+    enable drag and drop ability of the panel
+ */
 function addDragAndDrop(){
-    $('#ImageCapturer-Panel')[0].addEventListener('dragover',function(event){
+    var panel = $('#ImageCapturer-Panel')[0];
+    panel.addEventListener('dragover',function(event){
         event.stopPropagation();
         event.preventDefault();
         event.dataTransfer.dropEffect = 'copy';
     },false);
     //JQuery doesn't work on this
-    $('#ImageCapturer-Panel')[0].addEventListener('drop',function(event){
+    panel.addEventListener('drop',function(event){
         event.stopPropagation();
         event.preventDefault();
         var frame = $('#ImageCapturer-ImageFrame');
-        var imageUrl = event.dataTransfer.getData('text/html');
-        var $iu = $(imageUrl);
-        var imageClone = $iu[1];
-        console.debug(imageClone);
+        var data = $(event.dataTransfer.getData('text/html'));
+        var imageClone = $(data[1]);
+        console.debug(data[1]);
         //check if the element is image
-        if ($(imageClone).is('img') === false){
+        if (imageClone.is('img') === false){
             return;
         }else{
             //empty the current one
             frame.empty();
+            $('#ImageCapturer-ImageDescription').empty();
         }
-        var cloneContainer = $('<div></div>').append(imageClone);
-        frame.append(cloneContainer);
-        cloneContainer.css({
-            position: 'absolute',
-            width: '100%',
-            height: '100%'
-        });
-        cloneContainer.css("left", (frame.width()-cloneContainer.width())/2);
-        cloneContainer.css("top", (frame.height()-cloneContainer.height())/2);
-        cloneContainer.css("top", (frame.height()-cloneContainer.height())/2);
-        $(imageClone).css({
-            position: 'absolute',
+        frame.append(imageClone);
+        imageClone.css({
             width : '100%',
             height : '100%'
         });
+        var imageText = getImageDescription(imageClone);
+        console.debug("Text: "+imageText);
+        addImageTextToPanel(imageText);
+        addCurrentURL();
     },false);
 }
 
 
 (function() {
-    var popup = createPanel();
-    addCloseButton(popup);
-    addImageFrame(popup);
+    var panel = createPanel();
+    addCloseButton();
+    addImageFrame();
+    addImageDescriptionFrame();
     addDragAndDrop();
+    promptDropAnimation();
 })();
